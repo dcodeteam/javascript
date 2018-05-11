96327d7254a2afd842061a0904262a94d6c98cb3
Umidbek Karimov <uma.karimov@gmail.com>
2018-05-11 21:16:00 +0500

    feat(eslint-config-base): Disable `import/prefer-default-export` rule.

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -31,6 +31,7 @@ module.exports = {
     "import/export": "off",
     "import/extensions": "off",
     "import/no-unresolved": "off",
+    "import/prefer-default-export": "off",
     "import/no-extraneous-dependencies": "off",
   },
 };
