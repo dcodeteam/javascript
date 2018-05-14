974d9fb6385ddd6a975686558d08311e5b60831a
Umidbek Karimov <uma.karimov@gmail.com>
2018-05-14 11:01:36 +0500

    fix(eslint-config-react): Put `jsx-a11y` rules under `rules` field.

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -17,26 +17,26 @@ module.exports = {
     "react/jsx-first-prop-new-line": "off",
     "react/prefer-stateless-function": "off",
     "react/jsx-boolean-value": ["error", "always"],
+
+    //
+    // eslint-plugin-jsx-a11y
+    //
+
+    "jsx-a11y/label-has-for": [
+      "error",
+      {
+        components: ["Label"],
+        required: { some: ["nesting", "id"] },
+      },
+    ],
+
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: [],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["noHref", "invalidHref", "preferButton"],
+      },
+    ],
   },
-
-  //
-  // eslint-plugin-jsx-a11y
-  //
-
-  "jsx-a11y/label-has-for": [
-    "error",
-    {
-      components: ["Label"],
-      required: { some: ["nesting", "id"] },
-    },
-  ],
-
-  "jsx-a11y/anchor-is-valid": [
-    "error",
-    {
-      components: [],
-      specialLink: ["hrefLeft", "hrefRight"],
-      aspects: ["noHref", "invalidHref", "preferButton"],
-    },
-  ],
 };
