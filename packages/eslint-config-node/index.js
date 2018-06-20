152d0c5b4bf1b981ec96c33dd4f495411a3a0478
Umidbek Karimov <uma.karimov@gmail.com>
2018-06-20 17:40:54 +0500

    feat(eslint-config-node): Use `eslint-plugin-node`.

--- packages/eslint-config-node/index.js
+++ packages/eslint-config-node/index.js
@@ -3,6 +3,9 @@
 module.exports = {
   env: { node: true },
   parserOptions: { sourceType: "script" },
+
+  plugins: ["node"],
+
   extends: ["@dc0de/eslint-config-base"],
 
   rules: {
@@ -27,5 +30,27 @@ module.exports = {
         optionalDependencies: false,
       },
     ],
+
+    //
+    // eslint-plugin-node
+    //
+
+    // Possible Errors
+    "node/no-extraneous-import": "off",
+    "node/no-extraneous-require": "off",
+    "node/no-missing-import": "off",
+    "node/no-missing-require": "off",
+    "node/no-unpublished-bin": "error",
+    "node/no-unpublished-import": "error",
+    "node/no-unpublished-require": "error",
+    "node/no-unsupported-features": "error",
+    "node/process-exit-as-throw": "error",
+    "node/shebang": "error",
+
+    // Best Practices
+    "node/no-deprecated-api": "error",
+
+    // Stylistic Issues
+    "node/exports-style": "error",
   },
 };
