9c3213eb91a3155f46a8a260c82eb42a1950b56a
Umidbek Karimov <uma.karimov@gmail.com>
2018-07-25 15:29:54 +0500

    feat(eslint-config-node): Bump up `eslint-plugin-node`.

--- packages/eslint-config-node/index.js
+++ packages/eslint-config-node/index.js
@@ -43,7 +43,9 @@ module.exports = {
     "node/no-unpublished-bin": "error",
     "node/no-unpublished-import": "error",
     "node/no-unpublished-require": "error",
-    "node/no-unsupported-features": "error",
+    "node/no-unsupported-features/es-builtins": "error",
+    "node/no-unsupported-features/es-syntax": "error",
+    "node/no-unsupported-features/node-builtins": "error",
     "node/process-exit-as-throw": "error",
     "node/shebang": "error",
 
@@ -52,5 +54,10 @@ module.exports = {
 
     // Stylistic Issues
     "node/exports-style": "error",
+    "node/prefer-global/buffer": "error",
+    "node/prefer-global/console": "error",
+    "node/prefer-global/process": "error",
+    "node/prefer-global/url-search-params": "error",
+    "node/prefer-global/url": "error",
   },
 };
