3f3b0e80b31a1f5858008014de3153f54475436f
Umidbek Karimov <uma.karimov@gmail.com>
2018-11-25 16:41:57 +0500

    chore: Reorgonize local eslint rules.

--- packages/eslint-config-base/__tests__/eslint-config-base.spec.js
+++ packages/eslint-config-base/__tests__/eslint-config-base.spec.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { initESLintTest } = require("test-tools");
 
 initESLintTest("eslint-config-base", require.resolve(".."));
--- packages/jest-preset-react/__tests__/jest-preset-react.spec.js
+++ packages/jest-preset-react/__tests__/jest-preset-react.spec.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { initJestPresetTest } = require("test-tools");
 
 initJestPresetTest("jest-preset-react", require.resolve("../jest-preset"));
