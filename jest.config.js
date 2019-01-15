0d44bb2312fdd28c7695f2b64bb867adfb6156a0
Umidbek Karimov <uma.karimov@gmail.com>
2019-01-15 16:35:41 +0500

    chore: Run tests from root.

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+  testEnvironment: "node",
+  roots: [
+    "<rootDir>/packages/eslint-config",
+    "<rootDir>/packages/eslint-config-base",
+    "<rootDir>/packages/eslint-config-node",
+    "<rootDir>/packages/eslint-config-react",
+    "<rootDir>/packages/eslint-config-react-native",
+    "<rootDir>/packages/jest-preset",
+    "<rootDir>/packages/jest-preset-react",
+  ],
+};
