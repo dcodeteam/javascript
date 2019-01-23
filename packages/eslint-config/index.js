3587f3c96bc37c25b88881c426f1eab5b4175a55
Umidbek Karimov <uma.karimov@gmail.com>
2019-01-23 15:19:21 +0500

    feat(eslint-config): Migrate to `@typescript-eslint`.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,13 +1,12 @@
 "use strict";
 
 module.exports = {
-  parser: "typescript-eslint-parser",
-
-  plugins: ["typescript"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
 
   settings: {
     "import/parsers": {
-      "typescript-eslint-parser": [".ts", ".tsx"],
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
     "import/resolver": {
       typescript: {},
