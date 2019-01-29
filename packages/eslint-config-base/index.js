4306e547c702771ec849d01e14265ad610b1e2e4
Umidbek Karimov <uma.karimov@gmail.com>
2019-01-29 15:12:47 +0500

    feat(eslint-config-base): Bump dependencies, update rules.

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -16,10 +16,81 @@ module.exports = {
     // eslint
     //
 
-    // Just use Prettier ©
+    //
+    // Possible Errors
+
+    // Disallow using an async function as a Promise executor.
+    "no-async-promise-executor": "error",
+
+    // Disallow assignment operators in conditional expressions.
+    "no-cond-assign": ["error", "except-parens"],
+
+    // Disallow the use of `console`.
+    "no-console": "error",
+
+    // Disallow constant expressions in conditions.
+    "no-constant-condition": "error",
+
+    // Disallow empty block statements except `catch`.
+    "no-empty": ["error", { allowEmptyCatch: true }],
+
+    // Disallow characters which are made with multiple code points in
+    // character class syntax.
+    "no-misleading-character-class": "error",
+
+    // Disallow assignments that can lead to race conditions due to usage of
+    // `await` or `yield`.
+    "require-atomic-updates": "error",
+
+    //
+    // Stylistic Issues
+
+    // Require named `function` expressions.
+    "func-names": ["error", "always"],
+
+    // Allow nested ternary expressions.
     "no-nested-ternary": "off",
 
-    // Remind about technical debt.
+    // Allow the unary operators `++` and `--`.
+    "no-plusplus": ["off", {}],
+
+    // Require or disallow padding lines between statements.
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+      { blankLine: "always", prev: "*", next: "return" },
+    ],
+
+    // Disallow using `Object.assign` with an object literal as the first
+    // argument and prefer the use of object spread instead.
+    "prefer-object-spread": "error",
+
+    //
+    // ECMAScript 6
+
+    // Require braces around arrow function bodies.
+    "arrow-body-style": [
+      "error",
+      "as-needed",
+      { requireReturnForObjectLiteral: false },
+    ],
+
+    // Require using arrow functions for callbacks.
+    "prefer-arrow-callback": [
+      "error",
+      { allowNamedFunctions: false, allowUnboundThis: true },
+    ],
+
+    //
+    // Best Practices
+
+    // Enforce consistent brace style for all control statements.
+    curly: ["error", "all"],
+
+    // Disallow the use of `alert`, `confirm`, and `prompt`.
+    "no-alert": "error",
+
+    // Disallow specified warning terms in comments.
     "no-warning-comments": [
       "warn",
       {
@@ -28,50 +99,26 @@ module.exports = {
       },
     ],
 
-    // Forbid to use built in logger.
+    //
+    // Variables
 
-    // Override `airbnb-eslint-config-base` warnings as errors.
-    "no-alert": "error",
-    "no-console": "error",
-    "func-names": ["error", "always"],
-    "no-constant-condition": "error",
-
-    // Override `airbnb-eslint-config` defaults.
-    "no-cond-assign": ["error", "except-parens"],
-
-    // Allow empty catch blocks.
-    "no-empty": ["error", { allowEmptyCatch: true }],
-
-    // Allow plusplus in for loops.
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-
-    // It's ok to use hoisted functions before their actual declaration.
+    // Allow the use of variables before they are defined.
     "no-use-before-define": ["error", { functions: false }],
-
-    // Force usage object spread over Object.assign.
-    "prefer-object-spread": "error",
-
-    // Potential bugs.
-    "require-atomic-updates": "error",
-    "no-async-promise-executor": "error",
-    "no-misleading-character-class": "error",
-
-    // Force usage of curly braces.
-    curly: ["error", "all"],
-
-    // Force newlines.
-    "newline-after-var": "error",
-    "newline-before-return": "error",
 
     //
     // eslint-plugin-import
     //
 
-    // We use `sort-import` to deal with styles.
+    //
+    // Style guide
+
+    // Do not ensure all imports appear before other statements.
     "import/first": ["off", "absolute-first"],
+
+    // Do not enforce a convention in module import order.
     "import/order": ["off", { groups: [["builtin", "external", "internal"]] }],
 
-    // We prefer named exports.
+    // Do not prefer a default export if module exports a single name.
     "import/prefer-default-export": "off",
   },
 };
